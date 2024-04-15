4c452d60487a558977f4c936cfb2064948f50649
LucasSteinbach <lucaswsb52@gmail.com>
2024-04-14 22:40:50 -0300

    corrigindo responsivo landingpage

--- src/components/Sobre.vue
+++ src/components/Sobre.vue
@@ -1,14 +1,13 @@
 <template>
     <div id="sobre" class="mt-3">
-        <div class="col-md-12 d-flex shadow">
-            <div class="col-md-6 d-flex flex-column align-items-center justify-content-center">
+        <div class="col-md-12 d-flex shadow flex-wrap">
+            <div class="col-md-6 d-flex flex-column align-items-center justify-content-center p-3">
                 <div class="col-md-10">
-                    
-                <h1 class="text-roxo">SOBRE A PSICO PRONTUÁRIOS</h1>
+                    <h1 class="text-roxo">SOBRE A PSICO PRONTUÁRIOS</h1>
 
-                <span class="">
-                    Otimize sua prática clínica com nosso sistema de prontuários online para psicólogos. Com nossa plataforma, você pode criar e gerenciar facilmente notificações para suas consultas. Receba lembretes diretamente em seu whatsapp e email, garantindo que você nunca perca uma sessão importante. Simplifique seu fluxo de trabalho e concentre-se no que realmente importa: o bem-estar de seus pacientes. Experimente agora e descubra como nossa solução pode elevar sua prática a novos patamares de eficiência e cuidados.
-                </span>
+                    <span class="">
+                        Otimize sua prática clínica com nosso sistema de prontuários online para psicólogos. Com nossa plataforma, você pode criar e gerenciar facilmente notificações para suas consultas. Receba lembretes diretamente em seu whatsapp e email, garantindo que você nunca perca uma sessão importante. Simplifique seu fluxo de trabalho e concentre-se no que realmente importa: o bem-estar de seus pacientes. Experimente agora e descubra como nossa solução pode elevar sua prática a novos patamares de eficiência e cuidados.
+                    </span>
                 </div>
             </div>
             <div class="col-md-6">
