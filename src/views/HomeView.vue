c0b9a8e76a1dbc68ae072a5ad35c3a4e0e7df319
LucasSteinbach <lucaswsb52@gmail.com>
2024-05-06 23:34:42 -0300

    colocando mais uma sessao na landingpage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,16 @@
     <Navbar />
     <Sobre />
     <Modalidades />
+    <Especialista />
     <LGPD />
     <Planos />
     <Footer />
+
+    <div class="botao-fixo">
+      <Button @click="falarEspecialista" class="p-3 botao-wpp" v-tooltip.left="'Fale com um especialista'">
+        <i class="fa-brands fa-whatsapp fa-2xl"></i>
+      </Button>
+    </div>
   </div>
 </template>
 
@@ -16,6 +23,8 @@ import Sobre from '@/components/Sobre.vue';
 import Footer from '@/components/Footer.vue';
 import Modalidades from '../components/Modalidades.vue';
 import LGPD from '../components/LGPD.vue';
+import Button from 'primevue/button';
+import Especialista from '@/components/Especialista.vue';
 
 export default {
   name: 'HomeView',
@@ -26,6 +35,26 @@ export default {
     Footer,
     Modalidades,
     LGPD,
+    Especialista,
+    Button,
+  },
+  methods: {
+    falarEspecialista() {
+      window.open('https://wa.me/5534992021394?text=Ol%C3%A1%2C+gostaria+de+agendar+uma+demonstra%C3%A7%C3%A3o+do+sistema+Psico+Prontu%C3%A1rios%21', '_blank');
+    }
   }
 }
 </script>
+
+<style>
+.botao-fixo {
+  position: fixed;
+  bottom: 80px;
+  right: 20px;
+}
+
+.botao-wpp {
+  border-radius: 50%;
+  height: 60px;
+}
+</style>
