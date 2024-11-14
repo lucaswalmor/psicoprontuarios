8b0c4ce8d8b6ea598ba27a3b17bcf2f00efa1c09
Lucas Steinbach <lucaswsb52@gmail.com>
2024-11-14 16:02:18 -0300

    atualizacoes psico prontuarios

--- src/main.js
+++ src/main.js
@@ -28,8 +28,8 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 import VueTheMask from 'vue-the-mask'
 
 // AXIOS BASE URL
-// axios.defaults.baseURL = 'https://psicoprontuarios.lksoftware.com.br/api/';
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
+axios.defaults.baseURL = 'https://psicoprontuarios.lksoftware.com.br/api/';
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
 
 const app = createApp(App)
 app.config.globalProperties.axios = axios;
