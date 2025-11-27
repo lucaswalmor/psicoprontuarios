0261100b60d6a3599ea350c25d036367dc024cbc
Lucas Steinbach <lucaswsb52@gmail.com>
2025-11-26 22:36:13 -0300

    correcoes

--- src/utils/planValidations.js
+++ src/utils/planValidations.js
@@ -1,6 +1,7 @@
 export default {
     hasAccessToModule(module) {
         const modulosPlano = JSON.parse(localStorage.getItem('modulosPlano') || '{}');
+        console.log('modulosPlano: ', modulosPlano);
         return modulosPlano[module] === true;
     },
     
