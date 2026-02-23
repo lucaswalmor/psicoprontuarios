09d2c961985f7ab78c262852b9cccb8634d3817c
Lucas Steinbach <lucaswsb52@gmail.com>
2026-02-23 14:39:44 -0300

    update API URL to use productionV1

--- src/config/environment.js
+++ src/config/environment.js
@@ -34,7 +34,7 @@ export const getApiUrl = () => {
                          window.location.hostname === 'localhost' || 
                          window.location.hostname === '127.0.0.1';
     
-    return isDevelopment ? config.apiUrls.development : config.apiUrls.production;
+    return isDevelopment ? config.apiUrls.development : config.apiUrls.productionV1;
 };
 
 export default config;
